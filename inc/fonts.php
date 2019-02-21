41ebda755b4a69e061deaec1a0efa5ef72464db5
hyoon <hailey@poietes.art>
2019-02-21 16:40:28 -0500

    Remove unused fonts

--- inc/fonts.php
+++ inc/fonts.php
@@ -1,7 +1,7 @@
 <?php
 
   function poietes_register_fonts() {
-    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Playfair+Display|Roboto:300,400,700|Roboto+Condensed:300,400,700', false ); 
+    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Playfair+Display|Roboto+Condensed:300,400,700', false ); 
   }
 
   function poietes_register_fontawesome(){
@@ -10,6 +10,3 @@
     
   add_action('wp_enqueue_scripts', 'poietes_register_fonts');
   add_action('wp_enqueue_scripts', 'poietes_register_fontawesome');
-
-?>
- 
