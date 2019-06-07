00d1cda059754a9e3dc0fa39dc61ba114b2fb642
hyoon <haileetw@gmail.com>
2019-06-07 18:34:26 -0400

    Fix: CleanWebpackPlugin bug

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,21 +1,21 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = env => {
-  const devMode = env.NODE_ENV === "development";
+  const devMode = env.NODE_ENV === 'development';
 
   return {
-    devtool: devMode ? "source-map" : "none",
+    devtool: devMode ? 'source-map' : 'none',
 
     mode: env.NODE_ENV,
 
     externals: {
-      jquery: "jQuery"
+      jquery: 'jQuery'
     },
 
     performance: {
@@ -30,12 +30,12 @@ module.exports = env => {
     },
 
     entry: {
-      app: ["./resources/assets/js/app.js", "./resources/assets/sass/app.scss"]
+      app: ['./resources/assets/js/app.js', './resources/assets/sass/app.scss']
     },
 
     output: {
-      filename: "js/[name].js",
-      path: path.resolve(__dirname, "../dist")
+      filename: 'js/[name].js',
+      path: path.resolve(__dirname, '../dist')
     },
 
     module: {
@@ -44,9 +44,9 @@ module.exports = env => {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"]
+              presets: ['@babel/preset-env']
             }
           }
         },
@@ -54,49 +54,47 @@ module.exports = env => {
           test: /\.(sa|sc|c)ss$/,
           use: [
             MiniCssExtractPlugin.loader,
-            "css-loader",
+            'css-loader',
             {
-              loader: "postcss-loader",
+              loader: 'postcss-loader',
               options: {
-                plugins: [require("autoprefixer")]
+                plugins: [require('autoprefixer')]
               }
             },
-            "sass-loader"
+            'sass-loader'
           ]
         }
       ]
     },
 
     plugins: [
-      new CleanWebpackPlugin([path.resolve(__dirname, "../dist")], {
-        root: path.resolve(__dirname, "../")
-      }),
+      new CleanWebpackPlugin(),
       new MiniCssExtractPlugin({
-        filename: "css/[name].css"
+        filename: 'css/[name].css'
       }),
       new CopyWebpackPlugin([
         {
-          context: path.resolve(__dirname, "../resources/assets/"),
+          context: path.resolve(__dirname, '../resources/assets/'),
           from: {
-            glob: "images/**/*",
+            glob: 'images/**/*',
             dot: false
           },
-          to: path.resolve(__dirname, "../dist/[path][name].[ext]")
+          to: path.resolve(__dirname, '../dist/[path][name].[ext]')
         }
       ]),
       new BrowserSyncPlugin(
         {
-          host: "localhost",
+          host: 'localhost',
           port: 3000,
-          proxy: "http://localhost:8888/projects/poietes_web",
+          proxy: 'http://localhost:8888/projects/poietes_web',
           files: [
-            "*.php",
-            "template-parts/**/*.php",
-            "resources/templates/**/*.php",
-            "resources/assets/js/**/*.js",
-            "resources/assets/sass/**/*.{sass,scss}",
-            "resources/assets/images/**/*.{jpg,jpeg,png,gif,svg}",
-            "resources/assets/fonts/**/*.{eot,ttf,woff,woff2,svg}"
+            '*.php',
+            'template-parts/**/*.php',
+            'resources/templates/**/*.php',
+            'resources/assets/js/**/*.js',
+            'resources/assets/sass/**/*.{sass,scss}',
+            'resources/assets/images/**/*.{jpg,jpeg,png,gif,svg}',
+            'resources/assets/fonts/**/*.{eot,ttf,woff,woff2,svg}'
           ]
         },
         {
